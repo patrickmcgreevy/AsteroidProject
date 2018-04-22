f0fb9dfe975210cafe16c4f0a39c898bd5661450
Patrick McGreevy <patrick.mcgreevy99@gmail.com>
2018-04-22 16:33:38 -0700

    working on movement and updated the ship png

--- Asteroid/Asteroid/main.cpp
+++ Asteroid/Asteroid/main.cpp
@@ -26,7 +26,7 @@ int main()
 
 	Laser l1(pLaserTexture, s1.getSlope(), s1.getPos());
 
-	bool kp = false; // key pressed bool
+	bool kp = false, wPressed = false, aPressed = false, dPressed = false; // key pressed bool
 	int forwardMotion = 0; // position updater
 	int cwRotate = 0;
 
@@ -36,38 +36,75 @@ int main()
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
+			}
 
-			if (event.type == sf::Event::KeyPressed) // key pressed loop from aofallon
+			else if (event.type == sf::Event::KeyPressed) // key pressed loop from aofallon
 			{
 				kp = true;
 				if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 				{
 					forwardMotion += 2;
-
+					wPressed = true;
 				}
-			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				kp = true;
+				else
+				{
+					wPressed = false;
+				}
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+				{
+					dPressed = true;
+				}
+				else
+				{
+					dPressed = false;
+				}
+
 				if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 				{
-					cwRotate += 5;
-					
+					aPressed = true;
+				}
+				else
+				{
+					aPressed = false;
 				}
 			}
+			//if (event.type == sf::Event::KeyPressed)
+			//{
+			//	kp = true;
+			//	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			//	{
+			//		cwRotate += 5;
+			//		
+			//	}
+			//}
 			if (event.type == sf::Event::KeyReleased)
 			{
-				kp = false;
+				if()
+				//kp = false;
 			} // end of key stuff borught in
 		}
 
 		window.clear();
 
-		for (int i = 0; i < forwardMotion; i++)
+		if (wPressed)
+		{
+			s1.move();
+		}
+		if (dPressed)
+		{
+			s1.rotateCW();
+		}
+		if (aPressed)
+		{
+			s1.rotateCounterCW();
+		}
+		/*for (int i = 0; i < forwardMotion; i++)
 			s1.move();
 		for (int i = 0; i < cwRotate; i++)
-			s1.rotateCW();
+			s1.rotateCW()*/;
 		l1.move();
 
 		window.draw(l1.getBody());
@@ -75,7 +112,7 @@ int main()
 		window.draw(s1.getBody());
 
 		a1.getBody().move(a1.getSlope());
-		s1.rotateCW();
+		//s1.rotateCW();
 		//s1.move();
 		window.display();
 		forwardMotion = 0;
