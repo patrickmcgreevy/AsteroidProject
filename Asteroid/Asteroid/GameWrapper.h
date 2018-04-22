734243b710a95ed690128ba9719b17b471fc8ca6
Patrick McGreevy <patrick.mcgreevy99@gmail.com>
2018-04-22 12:05:27 -0700

    Started game wrapper

--- Asteroid/Asteroid/GameWrapper.h
+++ Asteroid/Asteroid/GameWrapper.h
@@ -2,11 +2,23 @@
 #include "Asteroid.h"
 #include "Laser.h"
 #include "Ship.h"
+#include "List.h"
 
 class GameWrapper
 {
 public:
 	GameWrapper();
 	~GameWrapper();
+
+	void splitAsteroid(Asteroid * oldAst); // Makes two new asteroids based on the parameter asteroid
+	bool checkSplit(Asteroid * oldAst); // Returns true or false depending on 
 private:
+	sf::Texture * mpAstText;
+	sf::Texture * mpLaserText;
+	//sf::Texture * mpShipText;
+	sf::RenderWindow mWindow;
+	
+	List<Asteroid *> mAstList;
+	List<Laser *> mLaserList;
+	sf::Event mEvent;
 };
