47775653163e47abb8e85e6a42f05030e76ed46e
spear <spearkajumulo@gmail.com>
2018-04-22 14:10:41 -0700

    added event loop.

--- Asteroid/Asteroid/main.cpp
+++ Asteroid/Asteroid/main.cpp
@@ -26,6 +26,10 @@ int main()
 
 	Laser l1(pLaserTexture, s1.getSlope(), s1.getPos());
 
+	bool kp = false; // key pressed bool
+	int forwardMotion = 0; // position updater
+	int cwRotate = 0;
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -33,10 +37,36 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::KeyPressed) // key pressed loop from aofallon
+			{
+				kp = true;
+				if (event.key.code == sf::Keyboard::W)
+				{
+					forwardMotion += 5;
+				}
+			}
+			if (event.type == sf::Event::KeyPressed)
+			{
+				kp = true;
+				if (event.key.code == sf::Keyboard::A)
+				{
+					cwRotate += 1;
+				}
+			}
+			if (event.type == sf::Event::KeyReleased)
+			{
+				kp = false;
+				s1.move();
+			} // end of key stuff borught in
 		}
 
 		window.clear();
 
+		for (int i = 0; i < forwardMotion; i++)
+			s1.move();
+		for (int i = 0; i < cwRotate; i++)
+			s1.rotateCW();
 		l1.move();
 
 		window.draw(l1.getBody());
@@ -44,9 +74,11 @@ int main()
 		window.draw(s1.getBody());
 
 		a1.getBody().move(a1.getSlope());
-		s1.rotateCW();
-		s1.move();
+		//s1.rotateCW();
+		//s1.move();
 		window.display();
+		forwardMotion = 0;
+		cwRotate = 0;
 	}
 	
 	delete pAsteroidTexture;
